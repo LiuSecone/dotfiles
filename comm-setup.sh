df706e6378ec0bfb4cfa0ffb2970c211550e1062
Sicong <liusicong1998@outlook.com>
2024-10-07 12:05:07 +0800

    add coom-setup.sh

---
+++ comm-setup.sh
@@ -0,0 +1,17 @@
+# vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+# zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# antigen
+export ANTIGEN="$HOME/.local/bin/antigen.zsh"
+mkdir -p "$HOME/.local/bin"
+curl -L git.io/antigen > "$ANTIGEN"
+
+cp ./.zshrc ~/.zshrc
+cp ./.antigenrc ~/.antigenrc
+
+chsh -s $(which zsh)
