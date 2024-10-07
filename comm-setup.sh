f187bc5a0f098b925954efae2bb5891667e70ada
Sicong <liusicong1998@outlook.com>
2024-10-07 12:57:19 +0800

    Fix need password require

--- comm-setup.sh
+++ comm-setup.sh
@@ -3,7 +3,8 @@ git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 # zsh
-sudo apt install zsh
+sudo apt update
+sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # antigen
@@ -14,4 +15,4 @@ curl -L git.io/antigen > "$ANTIGEN"
 cp ./.zshrc ~/.zshrc
 cp ./.antigenrc ~/.antigenrc
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
